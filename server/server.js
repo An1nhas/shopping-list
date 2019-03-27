542d988c7c5abab1a68460182c9bf2eb932b64d6
An1nhas <38216332+An1nhas@users.noreply.github.com>
2019-03-27 16:09:01 +0100

    server settings changed

--- server/server.js
+++ server/server.js
@@ -2,22 +2,12 @@ const express = require('express');
 const socket = require('socket.io');
 
 let shoppingArray = [];
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-
-
 
 //app setup
 const app = express();
-<<<<<<< HEAD
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log("server listening");
 });
-=======
-const server = app.listen(port, () => console.log('Sever running on port '+port));
->>>>>>> 66d7637c9956b3a1006e25a2b09de39e9de21bc5
 
 //static files
 app.use(express.static('public'));
